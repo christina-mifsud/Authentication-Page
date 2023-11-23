e5138bc92f633c3c5e633551b7b99dccc73240bd
christina-mifsud <christina.mifsud@gmail.com>
2023-11-23 21:47:35 +0000

    Auth context setup

--- auth-page/src/components/App.js
+++ auth-page/src/components/App.js
@@ -1,17 +1,20 @@
+import { AuthProvider } from "../contexts/AuthContext";
 import "./App.css";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Signup />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
