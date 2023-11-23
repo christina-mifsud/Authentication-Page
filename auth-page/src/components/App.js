f2e6eb82b8cfe35e392af6f1b7afb18cdbca50be
christina-mifsud <christina.mifsud@gmail.com>
2023-11-23 22:24:27 +0000

    Routing setup

--- auth-page/src/components/App.js
+++ auth-page/src/components/App.js
@@ -1,20 +1,27 @@
 import { AuthProvider } from "../contexts/AuthContext";
 import "./App.css";
 import Signup from "./Signup";
+import Dashboard from "./Dashboard";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
